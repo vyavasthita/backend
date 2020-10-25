aed330d228de6d0067daa9ae0452e4edb4b09866
Ubuntu <ubuntu@ip-172-31-33-162.ap-south-1.compute.internal>
2020-10-25 06:02:37 +0000

    update settings

--- backend/settings.py
+++ backend/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'appdatastore',
+    'USER': 'admin',
+    'PASSWORD': 'adminsharma',
+    'HOST': 'appdata.c0hrjtq8kw9y.ap-south-1.rds.amazonaws.com',
+    'PORT': '3306',
     }
 }
 
